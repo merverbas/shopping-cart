2d56b44f915449bebaedff996a157c114395387a
Merve Erbaş <merverbas40@gmail.com>
2021-11-22 13:29:18 +0300

    add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import "./styles.css";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+
+
+import { data } from "./data";
+
+export const BooksContext = createContext();
+
+export default function App() { 
+
+  const [state, setState] = useState({
+    bookList: data,
+    cart: []
+  })
+
+  const addToCart = book => setState({
+    ...state,
+    cart: state.cart.find(cartItem => cartItem.id === book.id)
+    ? state.cart.map(cartItem => cartItem.id === book.id
+    ? {...cartItem, count:cartItem.count +1 } : cartItem)
+    : [...state.cart, {...book, count: 1 }]
+
+  })
+
+  const removeFromCart = id => setState({
+    ...state,
+    cart: state.cart.filter(cartItem => cartItem.id !== id)
+  })
+
+  const increase = id =>{
+    setState({
+      ...state,
+      cart: state.cart.map(cartItem => cartItem.id === id ? {... cartItem, count:
+      cartItem.count +1} : cartItem)
+    })
+  }
+
+  const decrease = id =>{
+    setState({
+      ...state,
+      cart: state.cart.map(cartItem => cartItem.id === id ? {... cartItem, count:
+      cartItem.count > 1 ? cartItem.count - 1 : 1 } : cartItem)
+    })
+  }
+
   return (
+    <BooksContext.Provider value={{ state: state, addToCart, increase, decrease,removeFromCart }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Routes>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
+      </Routes>
     </div>
+    </BooksContext.Provider>
   );
 }
-
-export default App;
